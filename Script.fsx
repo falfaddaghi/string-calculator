271da8205580d1eedbe6ebad6f92f325723d81c8
sudqi <sudqiomar@gmail.com>
2017-05-31 17:30:43 +0300

    add unkonw amount of numbers:

--- Script.fsx
+++ Script.fsx
@@ -1,10 +1,14 @@
-﻿let addTwoNumbers (text:string)=
+﻿let rec addIntegers numbers=
+   match numbers with
+   |[]->0
+   |[first]->first
+   |first::rest-> first+addIntegers rest
+
+let addTwoNumbers (text:string)=
    let numbers= match text with
     |""->[0]
     |_->text.Split[|','|] |>Array.map System.Int32.Parse |>Array.toList
-   match numbers with
-   |[first]->first
-   |[first;second]->first+second
+   addIntegers numbers
 
 
    
