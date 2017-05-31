7c9b19e8b58fe4ca61978aa809a44161e205efea
sudqi <sudqiomar@gmail.com>
2017-05-31 16:51:30 +0300

    add 2 numbers

--- Script.fsx
+++ Script.fsx
@@ -1,1 +1,10 @@
-﻿
+﻿let addTwoNumbers (text:string)=
+   let numbers= match text with
+    |""->[|0|]
+    |_->text.Split[|','|] |>Array.map System.Int32.Parse 
+
+   match numbers with
+   |[|first|]->first
+   |[|first;second|]->first+second
+   
+
