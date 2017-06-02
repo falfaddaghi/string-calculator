8c90ff54e1fce82233f4381ddcc9cfc36b81b204
sudqi <sudqiomar@gmail.com>
2017-06-02 20:34:52 +0300

    dilimter with variable length

--- Script.fsx
+++ Script.fsx
@@ -10,7 +10,7 @@ let addTwoNumbers (text:string) d=
    let numbers= match text with
     |""->[0]
     |_->
-        let integers=text.Split[|d;'\n'|]|>Array.map System.Int32.Parse |>Array.filter(fun n->n<1000)
+        let integers=text.Split[|d;'\n'|]|>Array.filter(fun s->match s with |""->false;|_->true)|>Array.map System.Int32.Parse |>Array.filter(fun n->n<=1000)
         let negative=Array.filter(fun n-> n<0) integers
         match negative with
         |[||]->integers|>Array.toList
@@ -19,9 +19,11 @@ let addTwoNumbers (text:string) d=
 
 let first (text:string)=
    let d,text'= match text with
-        |x when text.StartsWith("//")->(text.Chars(2),text.Substring(4))
+        |x when text.StartsWith("//")->match text.Chars(3) with 
+            |'\n'->(text.Chars(2),text.Substring(4))
+            |_->failwith("invalid pattern")
         |_->(';',text)
    addTwoNumbers text' d
 
-let text="//,\n1,22,3,1000"
+let text="//*\n2*22*3****1000"
 first text
