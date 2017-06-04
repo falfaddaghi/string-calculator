e0609d8d7e3e9721647df7bc66a014fc6df3d8a7
sudqi <sudqiomar@gmail.com>
2017-06-04 12:57:48 +0300

    delete addIntegers and first functions

--- Script.fsx
+++ Script.fsx
@@ -1,47 +1,9 @@
-﻿let rec addIntegers numbers = 
-    match numbers with
-    | [] -> 0
-    | [ first ] -> first
-    | first :: rest -> first + addIntegers rest
-
-let sprintArray (arr : int []) = 
+﻿let sprintArray (arr : List<int>) = 
     let error = ""
-    arr |> Array.fold (fun acc elem -> 
+    arr |> List.fold (fun acc elem -> 
                match acc with
                | "" -> elem.ToString()
                | _ -> acc + "," + elem.ToString()) error
-
-let addTwoNumbers (text : string) d = 
-    let numbers = 
-        match text with
-        | "" -> [ 0 ]
-        | _ -> 
-            let integers = 
-                text.Split [| d; '\n' |]
-                |> Array.filter (fun s -> 
-                       match s with
-                       | "" -> false
-                       | _ -> true)
-                |> Array.map System.Int32.Parse
-                |> Array.filter (fun n -> n <= 1000)
-            
-            let negative = Array.filter (fun n -> n < 0) integers
-            match negative with
-            | [||] -> integers |> Array.toList
-            | _ -> failwith (sprintf "negative not allowed %s" (sprintArray negative))
-    addIntegers numbers
-
-let first (text : string) = 
-    let d, text' = 
-        match text with
-        | x when text.StartsWith("//") -> 
-            match text.Chars(2) with
-            | '[' -> 
-                let index = text.IndexOf(']')
-                (text.Substring(3, index - 3), text.Substring(index + 1))
-            | _ -> failwith ("invalid pattern")
-        | _ -> (";", text)
-    d
 
 let validIndex (dIndex:string*int)=
     let d,index=dIndex
@@ -53,7 +15,7 @@ let indexKey dIndex=
 let rec split (text : string)  (d:List<string>)  = 
     let myNumbers = 
         match text with
-        | "" -> []
+        | "" -> [""]
         | _ -> 
             let indexies= List.map(fun (x:string)-> x,text.IndexOf(x)) d|>List.filter validIndex|>List.sortBy indexKey
             match indexies with 
@@ -75,19 +37,45 @@ let rec getdelimiterList (text:string) =
 let getdelimitersWithText (text:string)=
     let newLineIndex=text.IndexOf("\n")
     let delimiterList=
-        if text.StartsWith("//") then
-           text.Substring(2,newLineIndex-2) |>getdelimiterList
-        else
-            [","]
-    (delimiterList,text.Substring(newLineIndex+1))
+            if text.StartsWith("//") then
+                match newLineIndex with
+                |(-1)->failwith("no new line after delimiter")
+                |_->text.Substring(2,newLineIndex-2) |>getdelimiterList
+            else
+                [",";"\n"]
+    ("\n"::delimiterList,text.Substring(newLineIndex+1))
 
+
+let calculate (text : string)  = 
+    let d,numberOfString=getdelimitersWithText  text
+    let numbers = 
+        match text with
+        | "" -> [ 0 ]
+        | _ -> 
+            let integers = split numberOfString d |> List.map System.Int32.Parse |> List.filter (fun n -> n <= 1000)
+            let negative = List.filter (fun n -> n < 0) integers
+            match negative with
+            | [] -> integers 
+            | _ -> failwith (sprintf "negative not allowed %s" (sprintArray negative))
+    List.sum numbers
 
     
 let t1 = "1,2"
 let t2 = "1,2\n3"
 let t3 = "//[;][**]\n1;2**3"
-let t4="1,,,2;3,,,4"
-let t5="1,2;3;4"
-t3.Substring(2,3)
-let d,text=getdelimitersWithText  t3
-let numbers=split text d |>List.map System.Int32.Parse|>List.sum
+let t4="//[,,,][;]\n1,,,2;3,,,4"
+let t5="//[;]\n1;2;3;4"
+let t6="//[;]\n1;2;3;4"
+let t7="//[;]\n1;2;3;1001"
+let t8="//[;]\n1;-1;3;4"
+let t9="//[;]\n123;2;1;"
+calculate ""
+calculate t1
+calculate t2
+calculate t3
+calculate t4
+calculate t5
+calculate t6
+calculate t7
+calculate t8
+calculate t9
